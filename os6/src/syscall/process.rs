977ceabdd214619cd7425918b0845741484dbd56
Yakkhini <59007159+Yakkhini@users.noreply.github.com>
2022-07-18 08:18:58 +0800

    Feat: Complete the sys_get_time funtion.

--- os6/src/syscall/process.rs
+++ os6/src/syscall/process.rs
@@ -1,16 +1,16 @@
 //! Process management syscalls
 
-use crate::mm::{translated_refmut, translated_ref, translated_str};
+use crate::config::MAX_SYSCALL_NUM;
+use crate::fs::{open_file, OpenFlags};
+use crate::mm::{translated_ref, translated_refmut, translated_str, self};
 use crate::task::{
     add_task, current_task, current_user_token, exit_current_and_run_next,
-    suspend_current_and_run_next, TaskStatus,
+    suspend_current_and_run_next, TaskStatus, self,
 };
-use crate::fs::{open_file, OpenFlags};
 use crate::timer::get_time_us;
+use alloc::string::String;
 use alloc::sync::Arc;
 use alloc::vec::Vec;
-use crate::config::MAX_SYSCALL_NUM;
-use alloc::string::String;
 
 #[repr(C)]
 #[derive(Debug)]
@@ -71,7 +71,6 @@ pub fn sys_exec(path: *const u8) -> isize {
     }
 }
 
-
 /// If there is not a child process whose pid is same as given, return -1.
 /// Else if there is a child process but it is still running, return -2.
 pub fn sys_waitpid(pid: isize, exit_code_ptr: *mut i32) -> isize {
@@ -111,14 +110,15 @@ pub fn sys_waitpid(pid: isize, exit_code_ptr: *mut i32) -> isize {
 
 // YOUR JOB: 引入虚地址后重写 sys_get_time
 pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
-    let _us = get_time_us();
-    // unsafe {
-    //     *ts = TimeVal {
-    //         sec: us / 1_000_000,
-    //         usec: us % 1_000_000,
-    //     };
-    // }
-    0
+    let us = get_time_us();
+    let ts_physic_address = mm::translated_refmut(task::current_user_token(), us);
+
+    *ts_physic_address = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+
+    return 0;
 }
 
 // YOUR JOB: 引入虚地址后重写 sys_task_info
@@ -126,7 +126,7 @@ pub fn sys_task_info(ti: *mut TaskInfo) -> isize {
     -1
 }
 
-// YOUR JOB: 实现sys_set_priority，为任务添加优先级
+// YOUR JOB: 实现 sys_set_priority，为任务添加优先级
 pub fn sys_set_priority(_prio: isize) -> isize {
     -1
 }
@@ -142,7 +142,7 @@ pub fn sys_munmap(_start: usize, _len: usize) -> isize {
 
 //
 // YOUR JOB: 实现 sys_spawn 系统调用
-// ALERT: 注意在实现 SPAWN 时不需要复制父进程地址空间，SPAWN != FORK + EXEC 
+// ALERT: 注意在实现 SPAWN 时不需要复制父进程地址空间，SPAWN != FORK + EXEC
 pub fn sys_spawn(_path: *const u8) -> isize {
     -1
 }
